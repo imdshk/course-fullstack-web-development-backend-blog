55a255e87579ed7c8c1c506179edb4f74d38e696
imdshk <83509028+imdshk@users.noreply.github.com>
2024-02-18 22:04:51 +0200

    refactored logger

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const config = require("./utils/config")
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const mongoose = require("mongoose")
 const blogsRouter = require("./controllers/blogs")
+const logger = require("./utils/logger")
+const mongoose = require("mongoose")
+
 
 mongoose.set("strictQuery", false)
 
@@ -12,10 +14,10 @@ const mongoUrl = config.MONGODB_URI
 mongoose
     .connect(mongoUrl)
     .then(() => {
-        console.log("Connected to Mongo DB")
+        logger.info("Connected to Mongo DB")
     })
     .catch((error) => {
-        console.log("Error connecting to Mongo DB", error.message)
+        logger.error("Error connecting to Mongo DB", error.message)
     })
 
 app.use(cors())
@@ -25,5 +27,5 @@ app.use("/api/blogs", blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    logger.info(`Server running on port ${PORT}`)
 })

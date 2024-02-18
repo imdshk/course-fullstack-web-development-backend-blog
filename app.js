dbe4101994d725dfa976bbb4c05925beb30b84c5
imdshk <83509028+imdshk@users.noreply.github.com>
2024-02-18 22:19:28 +0200

    exercise 4.2 completed

---
+++ app.js
@@ -0,0 +1,33 @@
+const config = require("./utils/config")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const blogsRouter = require("./controllers/blogs")
+const logger = require("./utils/logger")
+const middleware = require("./utils/middleware")
+const mongoose = require("mongoose")
+
+
+mongoose.set("strictQuery", false)
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose
+    .connect(mongoUrl)
+    .then(() => {
+        logger.info("Connected to Mongo DB")
+    })
+    .catch((error) => {
+        logger.error("Error connecting to Mongo DB", error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use("/api/blogs", blogsRouter)
+
+app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,31 +1,6 @@
+const app = require("./app")
 const config = require("./utils/config")
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const blogsRouter = require("./controllers/blogs")
 const logger = require("./utils/logger")
-const middleware = require("./utils/middleware")
-const mongoose = require("mongoose")
-
-
-mongoose.set("strictQuery", false)
-
-const mongoUrl = config.MONGODB_URI
-
-mongoose
-    .connect(mongoUrl)
-    .then(() => {
-        logger.info("Connected to Mongo DB")
-    })
-    .catch((error) => {
-        logger.error("Error connecting to Mongo DB", error.message)
-    })
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use("/api/blogs", blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {

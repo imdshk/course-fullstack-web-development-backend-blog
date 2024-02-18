d80d8eef33da7e317c689abd1e94c833de4a02eb
imdshk <83509028+imdshk@users.noreply.github.com>
2024-02-18 23:08:08 +0200

    exercise 4.4 completed

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (totalLikes, blog) => {
+        return totalLikes + blog.likes
+    }
+
+    return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
